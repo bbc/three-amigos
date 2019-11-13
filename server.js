72f8019d0aee89dc41110dfc92f8b8765c4d8c68
rodu <remi.oduyemi@bbc.co.uk>
2019-11-13 19:14:47 +0000

    merge into toms-branch

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ var app = express();
 var storyModel = require('./storyModel.json')
 const cors = require('cors')
 var number = 0;
-
+const { handleEmotion } = require('./emotion');
 app.use(cors());
 app.use(express.static(__dirname + '/www'));
 
 app.listen('3000');
 console.log('working on 3000');
+
+const emotions = {
+    'neutral': 0,
+    'sad': 0,
+    'angry': 0,
+    'happy': 0
+}
 
 app.get('/getStory', function (req, res) {
     
@@ -21,4 +28,17 @@ app.get('/getStory', function (req, res) {
 app.get('/nextStory', function (req, res) {
     
     res.send(`hello`);
+})
+
+app.get('/emotion/:emotion', (req, res) => {
+    const { emotion } = req.params;
+    console.log(`emotion=${emotion}`);
+    emotions[emotion] += 1;
+    console.log(`emotions=${JSON.stringify(emotions)}`);
+    res.send(200)
+});
+
+app.get('/getEmotions', (req, res) => {
+    res.send(emotions);
+    Object.keys(emotions).forEach(emotion => emotions[emotion] = 0)
 })
