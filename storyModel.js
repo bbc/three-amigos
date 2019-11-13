74681cfa7f74a365a34d3355780ff997923e4a2e
rodu <remi.oduyemi@bbc.co.uk>
2019-11-13 12:56:04 +0000

    add initial story model

---
+++ storyModel.js
@@ -0,0 +1,51 @@
+[
+    {
+        "headline": "Grace Millane trial: CCTV shows accused wheeling body in suitcase",
+        "condensed": "A jury has been shown CCTV of a man accused of murdering a backpacker pushing a suitcase said to contain her body.",
+        "long": "Grace Millane from Wickford, Essex, died the night before her 22nd birthday while travelling in New Zealand. A man went on a date with Ms Millane the day before leaving his Auckland hotel with two suitcases. Prosecutors claim the suitcase was then buried in woodland outside the city and Ms Millane was in one of them. After Ms Millane's death the man washed the rental car and left the shovel at the car wash. He was also seen putting items, believed to be Grace's personal effects, in a bin in an Auckland park.",
+        "questions": [
+            {
+                "question": "Who is the suspect?",
+                "answer": "The suspect can't be named for legal reasons but they deny murder."
+            },
+            {
+                "question": "Who is Grace Millane?",
+                "answer": "Grace Millane is a University of Lincoln graduate who died on December 1st 2018 while backpacking in Auckland, New Zealand."
+            }
+        ],
+        "funFacts": [
+            "A woman went on a date with the suspect a few days after Grace went missing and described him as \"intense\"."
+        ]
+    },
+{
+        "headline": "Lib Dem seat row escalates",
+        "condensed": "A Lib Dem candidate is threatening to stand down if his party does not apologise to one of his colleagues. On Tuesday, fellow party member Tim Walker announced he would not run in Canterbury against Labour's Rosie Duffield. Guy Kiddey says he will step aside unless the party stops disciplinary proceedings against Mr Walker and 'apologises unreservedly' to him.",
+        "long": 
+        [
+            "A Lib Dem candidate is threatening to stand down if his party does not apologise to one of his colleagues.",
+            "On Tuesday, fellow party member Tim Walker announced he would not run in Canterbury against Labour's Rosie Duffield, as he was worried about splitting the Remain vote and letting the Tories take the seat",
+            "Ms Duffield won the constituency from the Tories at the 2017 general election with a majority of only 187 votes.",
+            "But despite his decision, the Lib Dems say they will select another candidate and still run in the Kent constituency.",
+            "Guy Kiddey, who is due to stand for the party in High Peak, says he will step aside unless the party stops disciplinary proceedings against Mr Walker and 'apologises unreservedly' to him",
+            "But he doubts the party will say sorry, and says he plans to resign his membership and vote Labour if they don't.",
+            "In a statement on Twitter, Mr Kiddey says: \"How many times do voters have to say they are sick of this style of politics before anyone listens?",
+            "In all the campaigning I have done so far, the thing people want more than anything else is the tirades and aggression to stop."
+        ],
+        "questions": [
+            {
+                "question": "Why would a candidate withdraw?",
+                "answer": 
+                [
+                    "In a general election, the UK's voters are invited to choose an MP for their area - one of 650 constituencies.",
+                    "The UK uses a first-past-the-post voting system, sometimes described as 'winner takes all'.",
+                    "It means the local candidate who receives the most votes becomes an MP - and the party with the most winning candidates nationally normally forms a government.",
+                    "Coming a good second does not help a party win any more power in Parliament.",
+                    "Tactical voting is perfectly legal in the UK. Voters are free to choose any candidate on the ballot paper, no matter what the reason is."
+                ]
+            }
+        ],
+        "funFacts": [
+            "Kensington has the smallest majority of just 20 votes"
+        ]
+    }
+]
