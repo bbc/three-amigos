61f989068eba4e13a86b4f387bb89502acbbd2e5
rodu <remi.oduyemi@bbc.co.uk>
2019-11-13 12:48:45 +0000

    initial commit

---
+++ emotionMapping.js
@@ -0,0 +1,30 @@
+const emotionMapping = {
+    "sleep": {
+        "words": [
+                "boring",
+                "bored",
+                "tedious",
+                "not exciting"
+        ],
+        "image": "assets/emoticon/bunny_sleep.gif"
+    },
+    "thanks": {
+        "words": [
+            "thank you",
+            "ta",
+            "yay",
+            "that's great",
+            "fantastic"
+        ],
+        "image": "assets/emoticon/bunny_thanks.gif"
+    },
+    "hug": {
+        "words": [
+            "that's sad",
+            "worrying",
+            "upsetting",
+            "scary"
+        ],
+        "image": "assets/emoticon/bunny_hug.gif"
+    }
+}
---
+++ speech.js
@@ -0,0 +1,93 @@
+var final_transcript = '';
+var recognizing = false;
+// const emotions = ['sleep', 'thanks', 'hug', 'blush']
+  
+if ('webkitSpeechRecognition' in window) {
+
+var recognition = new webkitSpeechRecognition();
+
+recognition.continuous = true;
+recognition.interimResults = true;
+
+recognition.onstart = function() {
+    console.log('now recognising');
+    recognizing = true;
+};
+
+recognition.onerror = function(event) {
+    console.log(event.error);
+};
+
+recognition.onend = function() {
+    recognizing = false;
+};
+
+recognition.onresult = function(event) {
+    var interim_transcript = '';
+    console.log('in onresult')
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+        } else {
+            interim_transcript += event.results[i][0].transcript;
+        }
+    }
+    console.log(`final_transcript=${final_transcript}`);
+    console.log(`interim_transcript=${interim_transcript}`);
+    matchEmotion(final_transcript);
+    final_transcript = capitalize(final_transcript);
+    final_span.innerHTML = linebreak(final_transcript);
+    interim_span.innerHTML = linebreak(interim_transcript);
+    };
+}
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+
+function matchEmotion(s) {
+    s.split(' ').forEach(word => {
+        console.log(`word=${word}`);
+        // if (emotions.includes(word)) {
+        console.log(`emotionMapping=${JSON.stringify(emotionMapping)}`);
+        if (word in emotionMapping) {
+            console.log(`${word in emotionMapping}`)
+            // console.log(`${word} in emotions`)
+            // setEmoticon(word);
+        } else {
+            Object.values(emotionMapping).map(({ words }) => {
+                const image = words.filter(e => e === word).map(e => e.image);
+                console.log(image)
+            })
+        }
+    })
+
+}
+
+// function setEmoticon (emotion) {
+//     const avatar = document.getElementById('avatar');
+//     avatar.src = `assets/emoticon/bunny_${emotion}.gif`;
+//     return;
+// }
+
+function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+function capitalize(s) {
+    return s.replace(s.substr(0,1), function(m) { return m.toUpperCase(); });
+}
+
+function startDictation(event) {
+    console.log(`recognizing=${recognizing}`);
+    console.log(recognition);
+    if (recognizing) {
+        console.log('about to stop recognizing');
+        recognition.stop();
+        return;
+    }
+    final_transcript = '';
+    recognition.lang = 'en-GB';
+    recognition.start();
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+}
